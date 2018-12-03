6e57afa50f93684cc75fef0c8dc9a11f6b330cae
jiraspiom <jiraspiom@yahoo.com.br>
2018-12-03 10:11:19 -0200

    Projeto criado

---
+++ LinqComLambda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqComLambda
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
