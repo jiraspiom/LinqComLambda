7fd413a6e08df0afd837401e711655ebd0ee329a
jiraspiom <jiraspiom@yahoo.com.br>
2018-12-03 10:27:53 -0200

    * Categoria.cs: criado as 2 entidades
    * Produto.cs:
    * LinqComLambda.csproj:

---
+++ LinqComLambda/Entidade/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LinqComLambda.Entidade
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Classificacao { get; set; }
+       
+    }
+}
---
+++ LinqComLambda/Entidade/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+namespace LinqComLambda.Entidade
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", "
+                + Nome + ", "
+                + Valor.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Categoria.Nome + ", "
+                + Categoria.Classificacao;
+        }
+    }
+}
